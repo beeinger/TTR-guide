12bbb37dd14aea9ecdd6dfc5dd72fa19ad7fcd14
beeinger <sulecki.maciek@gmail.com>
2023-03-11 15:20:30 +0000

    added my rustlings progress, currently 95.7%

---
+++ rustlings/exercises/move_semantics/move_semantics4.rs
@@ -0,0 +1,26 @@
+// move_semantics4.rs
+// Refactor this code so that instead of passing `vec0` into the `fill_vec` function,
+// the Vector gets created in the function itself and passed back to the main
+// function.
+// Execute `rustlings hint move_semantics4` or use the `hint` watch subcommand for a hint.
+
+fn main() {
+    let mut vec = fill_vec();
+
+    println!("{} has length {} content `{:?}`", "vec", vec.len(), vec);
+
+    vec.push(88);
+
+    println!("{} has length {} content `{:?}`", "vec", vec.len(), vec);
+}
+
+// `fill_vec()` no longer takes `vec: Vec<i32>` as argument
+fn fill_vec() -> Vec<i32> {
+    let mut vec = Vec::new();
+
+    vec.push(22);
+    vec.push(44);
+    vec.push(66);
+
+    vec
+}
