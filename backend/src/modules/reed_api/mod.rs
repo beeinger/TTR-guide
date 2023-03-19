c12752ace47d529a666cb3aee6906cac1339ad98
beeinger <sulecki.maciek@gmail.com>
2023-03-19 17:49:04 +0000

    added get_jobs_previews

--- backend/src/modules/reed_api/mod.rs
+++ backend/src/modules/reed_api/mod.rs
@@ -1,8 +1,9 @@
+use self::types::{JobDetails, JobPreview, JobSearchResult};
 use super::config::Config;
 
 pub mod types;
 
-pub async fn get_job_details(job_id: u32) -> Result<types::JobDetails, Box<dyn std::error::Error>> {
+pub async fn get_job_details(job_id: u32) -> Result<JobDetails, Box<dyn std::error::Error>> {
     let url = format!("https://www.reed.co.uk/api/1.0/jobs/{}", job_id);
     let client = reqwest::Client::new();
     let response = client
@@ -16,4 +17,26 @@ pub async fn get_job_details(job_id: u32) -> Result<types::JobDetails, Box<dyn s
     }
 
     Ok(response.json().await?)
+}
+
+pub async fn get_jobs_previews(
+    skip_first: Option<u32>,
+) -> Result<Vec<JobPreview>, Box<dyn std::error::Error>> {
+    let url = format!(
+        "https://www.reed.co.uk/api/1.0/search?resultsToSkip={}",
+        skip_first.unwrap_or(0)
+    );
+    let client = reqwest::Client::new();
+    let response = client
+        .get(url)
+        .basic_auth(Config::from_env().reed_api_key, Option::<&str>::None)
+        .send()
+        .await?;
+
+    if !response.status().is_success() {
+        return Err(format!("Error: {}", response.status()).into());
+    }
+
+    let search_result: JobSearchResult = response.json().await?;
+    Ok(search_result.results)
 }
