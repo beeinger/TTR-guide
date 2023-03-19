db735b00be14d89726a351bd5ef78b5b3bbf8d27
beeinger <sulecki.maciek@gmail.com>
2023-03-19 22:54:44 +0000

    added fetching 100 posts in data_collector

--- backend/src/bin/data-collector.rs
+++ backend/src/bin/data-collector.rs
@@ -1,13 +1,35 @@
+use api::modules::reed_api::{get_job_details, get_jobs_previews, types::JobDetails};
+use futures::future::join_all;
 use lambda_runtime::{run, service_fn, Error, LambdaEvent};
 use serde::Deserialize;
+use std::collections::HashMap;
 
 /// This is the main body for the function.
 /// Write your code inside it.
 /// There are some code example in the following URLs:
 /// - https://github.com/awslabs/aws-lambda-rust-runtime/tree/main/examples
 async fn function_handler(_event: LambdaEvent<IgnoreEvent>) -> Result<String, Error> {
-    let job_details = "Hello, world!".to_string();
-    tracing::info!("{}", serde_json::to_string(&job_details).unwrap());
+    let jobs_previews = match get_jobs_previews(Some(0)).await {
+        Ok(jobs) => jobs,
+        Err(e) => {
+            tracing::error!("Error getting jobs previews: {}", e);
+            return Err(format!("Error getting jobs previews {}", e).into());
+        }
+    };
+    let tasks = jobs_previews.iter().map(|job| get_job_details(job.job_id));
+    let detailed_jobs: HashMap<u32, JobDetails> = join_all(tasks)
+        .await
+        .into_iter()
+        .filter_map(|result| match result {
+            Ok(job_details) => Some((job_details.job_id, job_details)),
+            Err(e) => {
+                tracing::error!("Error getting job details: {}", e);
+                None
+            }
+        })
+        .collect();
+
+    tracing::info!("Got {} jobs", detailed_jobs.len());
 
     Ok("Success".to_string())
 }
