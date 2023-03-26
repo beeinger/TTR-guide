cb72e1b53afd767d26b97fccb09cb877e2a9e70b
beeinger <sulecki.maciek@gmail.com>
2023-03-26 02:21:32 +0100

    proces job post sqs

---
+++ backend/src/modules/process_job/mod.rs
@@ -0,0 +1,31 @@
+use super::{db, reed_api::types::JobDetails};
+
+pub mod sqs;
+pub mod types;
+
+pub async fn process_job(job: JobDetails) -> Result<(), Box<dyn std::error::Error>> {
+    let job_description = match job.job_description.clone() {
+        Some(description) => description,
+        None => {
+            tracing::warn!("Job description is empty");
+            return Ok(());
+        }
+    };
+    let processed_job = match super::gpt::extract_job_details(&job_description.as_str()).await {
+        Ok(job) => job,
+        Err(e) => {
+            tracing::error!("Error processing job: {:?}", e);
+            return Ok(());
+        }
+    };
+    let job_update = JobDetails {
+        position: processed_job.position,
+        technologies: processed_job.technologies,
+        work_flexibility: processed_job.work_flexibility,
+        processed: Some(true),
+        ..job
+    };
+    db::put_many_job_posts(Vec::from([job_update])).await?;
+
+    Ok(())
+}
---
+++ backend/src/modules/process_job/sqs.rs
@@ -0,0 +1,39 @@
+use rusoto_core::Region;
+use rusoto_sqs::{SendMessageBatchRequest, SendMessageBatchRequestEntry, Sqs, SqsClient};
+
+use crate::modules::{config, reed_api::types::JobDetails};
+use uuid::Uuid;
+
+use super::types::ProcessJobMessage;
+
+pub async fn add_jobs_to_sqs(jobs: Vec<JobDetails>) -> Result<(), Box<dyn std::error::Error>> {
+    let sqs_client = SqsClient::new(Region::default());
+    let queue_url = config::get_process_job_queue_url();
+
+    let chunk_size = 10;
+    let chunks = jobs.chunks(chunk_size);
+
+    for chunk in chunks {
+        let mut entries = Vec::new();
+        for job in chunk {
+            let message = ProcessJobMessage { job: job.clone() };
+            let message_body = serde_json::to_string(&message)?;
+            let entry = SendMessageBatchRequestEntry {
+                id: Uuid::new_v4().to_string(),
+                message_body,
+                ..Default::default()
+            };
+            entries.push(entry);
+        }
+
+        let request = SendMessageBatchRequest {
+            entries,
+            queue_url: queue_url.clone(),
+            ..Default::default()
+        };
+
+        sqs_client.send_message_batch(request).await?;
+    }
+
+    Ok(())
+}
